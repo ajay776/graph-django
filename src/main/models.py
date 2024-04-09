8a4f910ebf57cf240427e86f41e9f65ac32dc2ba
ajay776 <vishwakarmaajay776@gmail.com>
2024-04-09 18:01:30 +0530

    first commit

---
+++ src/main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=250)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="ingredients", on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
